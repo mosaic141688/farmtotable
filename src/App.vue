b8f57adbea7ad2ffda4a6c987c4cde177d76d444
Sihle Mbhamali <mbhamalimosaic@gmail.com>
2019-12-26 18:21:47 +0200

    Adding stuff on the website

--- src/App.vue
+++ src/App.vue
@@ -10,19 +10,11 @@
           alt="Vuetify Logo"
           class="shrink mr-2"
           contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          :src="require('./assets/logo_transparent_background.webp')"
           transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
           width="100"
         />
+Farm To Table
       </div>
 
       <v-spacer></v-spacer>
@@ -32,25 +24,21 @@
         target="_blank"
         text
       >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
+        <span class="mr-2">Login</span>
       </v-btn>
     </v-app-bar>
 
     <v-content>
-      <HelloWorld/>
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
   },
 
   data: () => ({
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -4,4 +4,13 @@ import Vuetify from 'vuetify/lib';
 Vue.use(Vuetify);
 
 export default new Vuetify({
+    theme: {
+        themes: {
+          light: {
+            primary: '#000000', // #E53935
+            secondary: '#000000', // #FFCDD2
+            accent: '#000000', // #3F51B5
+          },
+        },
+      },
 });

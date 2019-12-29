932494c35aa280edc3f761ce7d299e1c2d935ea4
Sihle Mbhamali <mbhamalimosaic@gmail.com>
2019-12-30 00:50:20 +0200

    PWA update

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,25 @@
 module.exports = {
   "transpileDependencies": [
     "vuetify"
-  ]
+  ],
+  pwa: {
+    name: 'My App',
+    themeColor: '#000000',
+    msTileColor: '#000000',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+
+    // configure the workbox plugin
+    workboxOptions: {
+      // swSrc is required in InjectManifest mode.
+      runtimeCaching: [{
+        // Match any same-origin request that contains 'api'.
+        urlPattern: /api/,
+        // Apply a network-first strategy.
+        handler: 'NetworkFirst'
+      }]
+      
+      // ...other Workbox options...
+    }
+  }
 }
